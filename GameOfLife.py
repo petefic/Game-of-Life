8867d4b60b0ea5017ebcf35fb1014af09251ec38
Pete Ficorelli <petefic@gmail.com>
2014-05-02 16:12:41 -0400

    changed cell list from 1d to 2d

--- GameOfLife.py
+++ GameOfLife.py
@@ -13,7 +13,8 @@ GREY = (30,30,30)
 SCREENX = 1280
 SCREENY = 720
 CELLSIZE = 10
-NUMOFCELLS = (SCREENX/CELLSIZE) * (SCREENY/CELLSIZE)
+ROWS = SCREENX/CELLSIZE
+COLS = SCREENY/CELLSIZE
 
 
 #set up window
@@ -22,37 +23,43 @@ pygame.display.set_caption('Game of Life')
 window.fill(BLACK)
 
 #generate random seed
-cells=[]
-for i in range(NUMOFCELLS):
-    r = randint(0,6)
-    if r == 0:
-        cells.append(1)
-    else:
-        cells.append(0)
+cells = [[None for i in range(COLS)] for i in range(ROWS)]
+for x in range(ROWS):
+	for y in range(COLS):
+	    r = randint(0,6)
+	    if r == 0:
+	        cells[x][y] = 1
+	    else:
+	        cells[x][y] = 0
 
 #draw grid to screen
 def drawGrid(SCREENX, SCREENY, CELLSIZE, cells):
-	currentCell=0
+	currentX=0
 	for x in range(0,SCREENX,CELLSIZE):
+		currentY=0
 		for y in range(0,SCREENY,CELLSIZE):
-			if cells[currentCell] == 1:
+			if cells[currentX][currentY] == 1:
 				#alive cell
 				pygame.draw.rect(window, RED, [x, y, CELLSIZE, CELLSIZE])
 			else:
 				#dead cell, just draw grid border
 				pygame.draw.rect(window, GREY, [x, y, CELLSIZE, CELLSIZE], 1)
-			currentCell+=1
+			currentY+=1
+		currentX+=1
 
 #main loop
 done = False
 while not done:
+	
 	#generations per second
 	clock.tick(10)
 
+	#check if user wants to exit
 	for event in pygame.event.get():
-		#check if user wants to exit
 	    if event.type == pygame.QUIT:
 	        done=True
+
+
 
 	drawGrid(SCREENX, SCREENY, CELLSIZE, cells)
 

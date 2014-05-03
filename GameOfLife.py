67ca714baa821642a878079628c5a94c4864e07b
Pete Ficorelli <petefic@gmail.com>
2014-05-03 16:21:22 -0400

    code cleanup

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,6 +1,7 @@
 import pygame, sys
 from pygame.locals import *
 from random import randint
+import numpy
 
 #inititalize
 pygame.init()
@@ -12,9 +13,9 @@ RED = (255,0,0)
 GREY = (30,30,30)
 SCREENX = 800
 SCREENY = 600
-CELLSIZE = 5
-ROWS = SCREENX/CELLSIZE
-COLS = SCREENY/CELLSIZE
+CELLSIZE = 10
+HEIGHT = SCREENY/CELLSIZE
+WIDTH = SCREENX/CELLSIZE
 
 
 #set up window
@@ -23,14 +24,12 @@ pygame.display.set_caption('Game of Life')
 window.fill(BLACK)
 
 #generate random seed
-cells = [[None for i in range(COLS)] for i in range(ROWS)]
-for x in range(0,ROWS):
-	for y in range(0,COLS):
+cells = numpy.zeros((WIDTH,HEIGHT))
+for x in range(0,WIDTH):
+	for y in range(0,HEIGHT):
 	    r = randint(0,15)
 	    if r == 0:
 	        cells[x][y] = 1
-	    else:
-	        cells[x][y] = 0
 
 #main loop
 done = False
@@ -45,59 +44,55 @@ while not done:
 	        done=True
 
 	#update cells
-	for x in range(0,ROWS):
-		for y in range(0,COLS):
+	for x in range(0,WIDTH):
+		for y in range(0,HEIGHT):
 			
 			#get neighbors
 			neighbors = []
-			if x!=0 and y!=COLS-1:
-				neighbors.append([x-1,y+1])
-			if y!=COLS-1:
-				neighbors.append([x,y+1])
-			if x!=ROWS-1 and y!=COLS-1:
-				neighbors.append([x+1,y+1])
+			if x!=0 and y!=HEIGHT-1:
+				neighbors.append(cells[x-1][y+1])
+			if y!=HEIGHT-1:
+				neighbors.append(cells[x][y+1])
+			if x!=WIDTH-1 and y!=HEIGHT-1:
+				neighbors.append(cells[x+1][y+1])
 			if x!=0:
-				neighbors.append([x-1,y])
-			if x!=ROWS-1:
-				neighbors.append([x+1,y])
+				neighbors.append(cells[x-1][y])
+			if x!=WIDTH-1:
+				neighbors.append(cells[x+1][y])
 			if x!=0 and y!=0:
-				neighbors.append([x-1,y-1])
+				neighbors.append(cells[x-1][y-1])
 			if y!=0:
-				neighbors.append([x,y-1])
-			if x!=ROWS-1 and y!=0:
-				neighbors.append([x+1,y-1])
+				neighbors.append(cells[x][y-1])
+			if x!=WIDTH-1 and y!=0:
+				neighbors.append(cells[x+1][y-1])
 
 			#determine # of living neighbors
-			alive = []
+			alive = 0
 			for i in neighbors:
-				if cells[i[0]][i[1]] == 1:
-					alive.append(i)
+				if i == 1:
+					alive+=1
 
 			#if current cell is dead
 			if cells[x][y] == 0:
 				#make alive if 3 alive neighbors
-				if len(alive) == 3:
+				if alive == 3:
 					cells[x][y] = 1
 			#if current cell is alive
-			else:
+			elif cells[x][y] == 1:
 				#kill if less than 2 or more than 3 alive neighbors
-				if (len(alive)<2)  | (len(alive)>3):
+				if (alive<2) or (alive>3):
 					cells[x][y] = 0
 	        
 	#draw grid
-	currentX=0
 	for x in range(0,SCREENX,CELLSIZE):
-		currentY=0
 		for y in range(0,SCREENY,CELLSIZE):
-			if cells[currentX][currentY] == 1:
+			if cells[x/CELLSIZE][y/CELLSIZE] == 1:
 				#alive cell
 				pygame.draw.rect(window, RED, [x, y, CELLSIZE, CELLSIZE])
 			else:
-				#dead cell, black square with grey border
+				#dead cell
 				pygame.draw.rect(window, BLACK, [x, y, CELLSIZE, CELLSIZE])
-				pygame.draw.rect(window, GREY, [x, y, CELLSIZE, CELLSIZE], 1)
-			currentY+=1
-		currentX+=1
+			pygame.draw.rect(window, GREY, [x, y, CELLSIZE, CELLSIZE], 1)
 
 	#draw updates
 	pygame.display.flip()

02783cf22f71efe37d79eca1874fee72d1f193a4
Pete Ficorelli <petefic@gmail.com>
2014-05-08 16:08:50 -0400

    simplified code

--- GameOfLife.py
+++ GameOfLife.py
@@ -8,11 +8,12 @@ pygame.init()
 clock = pygame.time.Clock()
 
 #constants
+FPS = 10
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREY = (30,30,30)
-SCREENX = 800
-SCREENY = 600
+SCREENX = 640
+SCREENY = 480
 CELLSIZE = 10
 HEIGHT = SCREENY/CELLSIZE
 WIDTH = SCREENX/CELLSIZE
@@ -24,77 +25,82 @@ pygame.display.set_caption('Game of Life')
 window.fill(BLACK)
 
 #generate random seed
-cells = numpy.zeros((WIDTH,HEIGHT))
+cells = numpy.zeros((WIDTH,HEIGHT), dtype=numpy.int)
 for x in range(0,WIDTH):
 	for y in range(0,HEIGHT):
-	    r = randint(0,15)
-	    if r == 0:
-	        cells[x][y] = 1
+	    cells[x][y] = randint(0,1)
+
+def findNeighbors(grid, x, y):
+    if 0 < x < len(grid) - 1:
+        xi = (0, -1, 1)
+    elif x > 0:
+        xi = (0, -1)
+    else:
+        xi = (0, 1)
+
+    yi = (0, -1, 1) if 0 < y < len(grid[0]) - 1 else ((0, -1) if y > 0 else (0, 1))
+    for a in xi:
+        for b in yi:
+            if a == b == 0:
+                continue
+            yield grid[x + a][y + b]
+
 
 #main loop
-done = False
-while not done:
-	
-	#generations per second
-	clock.tick(7)
+while True:
 
 	#check if user wants to exit
 	for event in pygame.event.get():
 	    if event.type == pygame.QUIT:
-	        done=True
+	        pygame.quit()
+	        sys.exit()
 
 	#update cells
 	for x in range(0,WIDTH):
 		for y in range(0,HEIGHT):
 			
 			#get neighbors
-			neighbors = []
-			if x!=0 and y!=HEIGHT-1:
-				neighbors.append(cells[x-1][y+1])
-			if y!=HEIGHT-1:
-				neighbors.append(cells[x][y+1])
-			if x!=WIDTH-1 and y!=HEIGHT-1:
-				neighbors.append(cells[x+1][y+1])
-			if x!=0:
-				neighbors.append(cells[x-1][y])
-			if x!=WIDTH-1:
-				neighbors.append(cells[x+1][y])
-			if x!=0 and y!=0:
-				neighbors.append(cells[x-1][y-1])
-			if y!=0:
-				neighbors.append(cells[x][y-1])
-			if x!=WIDTH-1 and y!=0:
-				neighbors.append(cells[x+1][y-1])
+			neighbors = findNeighbors(cells,x,y)
 
-			#determine # of living neighbors
+			#determine num of living neighbors
 			alive = 0
 			for i in neighbors:
 				if i == 1:
 					alive+=1
 
+			
+			#if current cell is alive
+			if cells[x][y] == 1:
+				#kill if less than 2 or more than 3 alive neighbors
+				if alive < 2:
+					cells[x][y] = 0
+				elif alive > 3:
+					cells[x][y] = 0
+				else:
+					cells[x][y] = 1
 			#if current cell is dead
-			if cells[x][y] == 0:
+			elif cells[x][y] == 0:
 				#make alive if 3 alive neighbors
 				if alive == 3:
 					cells[x][y] = 1
-			#if current cell is alive
-			elif cells[x][y] == 1:
-				#kill if less than 2 or more than 3 alive neighbors
-				if (alive<2) or (alive>3):
+				else:
 					cells[x][y] = 0
-	        
+			
 	#draw grid
 	for x in range(0,SCREENX,CELLSIZE):
 		for y in range(0,SCREENY,CELLSIZE):
+			#if cell is alive
 			if cells[x/CELLSIZE][y/CELLSIZE] == 1:
-				#alive cell
+				#draw red square
 				pygame.draw.rect(window, RED, [x, y, CELLSIZE, CELLSIZE])
 			else:
-				#dead cell
+				#draw black square
 				pygame.draw.rect(window, BLACK, [x, y, CELLSIZE, CELLSIZE])
+			#draw square border
 			pygame.draw.rect(window, GREY, [x, y, CELLSIZE, CELLSIZE], 1)
 
 	#draw updates
-	pygame.display.flip()
+	pygame.display.update()
 
-pygame.quit()
+	#generations per second
+	clock.tick(FPS)

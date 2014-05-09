7126a16182c779ecff1ae650f8c6ec5fee547c7e
Pete Ficorelli <petefic@gmail.com>
2014-05-08 20:10:07 -0400

    added options user can set

--- GameOfLife.py
+++ GameOfLife.py
@@ -4,23 +4,26 @@ from random import randint
 import numpy
 from copy import copy, deepcopy
 
+#get settings from user
+fps = int(raw_input("Generations per second: "))
+screenx = int(raw_input("Screen width: "))
+screeny = int(raw_input("Screen height: "))
+cellsize = int(raw_input("Cell size: "))
+seedamt = int(raw_input("Seed amount: "))
+
 #inititalize
 pygame.init()
 clock = pygame.time.Clock()
 
 #constants
-FPS = 10
 BLACK = (0,0,0)
 RED = (255,0,0)
 GREY = (30,30,30)
-SCREENX = 640
-SCREENY = 480
-CELLSIZE = 10
-HEIGHT = SCREENY/CELLSIZE
-WIDTH = SCREENX/CELLSIZE
+HEIGHT = screeny/cellsize
+WIDTH = screenx/cellsize
 
 #set up window
-window = pygame.display.set_mode((SCREENX, SCREENY))
+window = pygame.display.set_mode((screenx, screeny))
 pygame.display.set_caption('Game of Life')
 window.fill(BLACK)
 
@@ -28,7 +31,8 @@ window.fill(BLACK)
 cells = numpy.zeros((WIDTH,HEIGHT), dtype=numpy.int)
 for x in range(0,WIDTH):
 	for y in range(0,HEIGHT):
-	    cells[x][y] = randint(0,1)
+		if randint(0,seedamt) == 0:
+	    		cells[x][y] = 1
 
 def findNeighbors(grid, x, y):
     if 0 < x < len(grid) - 1:
@@ -91,20 +95,20 @@ while True:
 	cells = deepcopy(new)
 			
 	#draw grid
-	for x in range(0,SCREENX,CELLSIZE):
-		for y in range(0,SCREENY,CELLSIZE):
+	for x in range(0,screenx,cellsize):
+		for y in range(0,screeny,cellsize):
 			#if cell is alive
-			if cells[x/CELLSIZE][y/CELLSIZE] == 1:
+			if cells[x/cellsize][y/cellsize] == 1:
 				#draw red square
-				pygame.draw.rect(window, RED, [x, y, CELLSIZE, CELLSIZE])
+				pygame.draw.rect(window, RED, [x, y, cellsize, cellsize])
 			else:
 				#draw black square
-				pygame.draw.rect(window, BLACK, [x, y, CELLSIZE, CELLSIZE])
+				pygame.draw.rect(window, BLACK, [x, y, cellsize, cellsize])
 			#draw square border
-			pygame.draw.rect(window, GREY, [x, y, CELLSIZE, CELLSIZE], 1)
+			pygame.draw.rect(window, GREY, [x, y, cellsize, cellsize], 1)
 
 	#draw updates
 	pygame.display.update()
 
 	#generations per second
-	clock.tick(FPS)
+	clock.tick(fps)
